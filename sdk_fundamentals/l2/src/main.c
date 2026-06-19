e50e673deb3d87641651f0851c713cf9acd65e04
Matias Marfisi <matimarfisi99@gmail.com>
2026-06-19 20:47:06 +0000

    feat: add polling based led control

--- sdk_fundamentals/l2/src/main.c
+++ sdk_fundamentals/l2/src/main.c
@@ -8,25 +8,27 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define L2_POLLING
+// #define L2_INTERRUPT
 
-/* The devicetree node identifier for the "led0" alias. */
+#define POLL_TIME_MS   100
+
 #define LED0_NODE DT_ALIAS(led0)
+#define SW0_NODE DT_ALIAS(sw0)
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
 int main(void)
 {
 	int ret;
-	bool led_state = true;
 
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
+	}
+
+	if (!gpio_is_ready_dt(&button)) {
+		return -1;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
@@ -34,15 +36,17 @@ int main(void)
 		return 0;
 	}
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret < 0) {
+		return -1;
+	}
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
+	bool val = false;
+
+	while (1) {
+		val = gpio_pin_get_dt(&button);
+		gpio_pin_set_dt(&led, val);
+		k_msleep(POLL_TIME_MS);
 	}
 	return 0;
 }
