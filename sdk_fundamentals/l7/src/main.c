0b044c53bf0b743de2b0ebf1a57e33054fc9bfe4
Matias Marfisi <matimarfisi99@gmail.com>
2026-06-25 16:12:43 +0000

    feat: add l7 e1 thread creation and yield/sleep

---
+++ sdk_fundamentals/l7/src/main.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+#define SLEEP_TIME_MS   	5
+
+// Threads defines
+#define STACKSIZE 1024
+#define THREAD0_PRIORITY 7
+#define THREAD1_PRIORITY 7
+
+void thread0(void)
+{
+	while (1) {
+		printk("Hello, I am thread0\n");
+		k_msleep(SLEEP_TIME_MS);
+	}
+}
+
+void thread1(void)
+{
+	while (1) {
+		printk("Hello, I am thread1\n");
+		k_msleep(SLEEP_TIME_MS);
+	}
+}
+
+K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL,
+ THREAD0_PRIORITY, 0, 0);
+K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL,
+ THREAD1_PRIORITY, 0, 0);
