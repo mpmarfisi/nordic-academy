4be8d991ab41b872ef02ed845b9fa7db0a6aa4cf
Matias Marfisi <matimarfisi99@gmail.com>
2026-06-25 16:22:09 +0000

    feat: add l7 e2 time slicing and priorities

--- sdk_fundamentals/l7/src/main.c
+++ sdk_fundamentals/l7/src/main.c
@@ -12,14 +12,16 @@
 
 // Threads defines
 #define STACKSIZE 1024
-#define THREAD0_PRIORITY 7
+#define THREAD0_PRIORITY 6
 #define THREAD1_PRIORITY 7
 
 void thread0(void)
 {
 	while (1) {
 		printk("Hello, I am thread0\n");
-		k_msleep(SLEEP_TIME_MS);
+		// k_yield();
+		// k_msleep(SLEEP_TIME_MS);
+		k_busy_wait(1000000);
 	}
 }
 
@@ -27,11 +29,11 @@ void thread1(void)
 {
 	while (1) {
 		printk("Hello, I am thread1\n");
-		k_msleep(SLEEP_TIME_MS);
+		// k_yield();
+		// k_msleep(SLEEP_TIME_MS);
+		k_busy_wait(1000000);
 	}
 }
 
-K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL,
- THREAD0_PRIORITY, 0, 0);
-K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL,
- THREAD1_PRIORITY, 0, 0);
+K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL, THREAD0_PRIORITY, 0, 0);
+K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL, THREAD1_PRIORITY, 0, 0);
