e410961b81b76268c2a6e42287e03ad7ffb03155
Matias Marfisi <matimarfisi99@gmail.com>
2026-06-30 17:32:25 +0000

    feat: add l1 e2 fifo exercise

--- sdk_intermediate/l1/src/main.c
+++ sdk_intermediate/l1/src/main.c
@@ -4,9 +4,11 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+#include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/random/random.h>
 
 /* The devicetree node identifier for the "led0"  and "led1" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -15,11 +17,24 @@
 /* 2200 msec = 2.2 sec */
 #define PRODUCER_SLEEP_TIME_MS 2200
 
-LOG_MODULE_REGISTER(Less1_Exer1, LOG_LEVEL_DBG);
-/* Stack size for both the producer and consumer threads */
-#define STACKSIZE		 2048
-#define PRODUCER_THREAD_PRIORITY 6
-#define CONSUMER_THREAD_PRIORITY 7
+#ifdef CONFIG_INTER_L1_MESSAGE_QUEUE
+	LOG_MODULE_REGISTER(Less1_Exer1, LOG_LEVEL_DBG);
+	/* Stack size for both the producer and consumer threads */
+	#define STACKSIZE		 2048
+	#define PRODUCER_THREAD_PRIORITY 6
+	#define CONSUMER_THREAD_PRIORITY 7
+#endif
+
+#ifdef CONFIG_INTER_L1_FIFO
+	LOG_MODULE_REGISTER(Less1_Exer2, LOG_LEVEL_DBG);
+	/* Stack size for both the producer and consumer threads */
+	#define STACKSIZE		 2048
+	#define PRODUCER_THREAD_PRIORITY 6
+	#define CONSUMER_THREAD_PRIORITY 7
+	#define MAX_DATA_SIZE		 32
+	#define MIN_DATA_ITEMS		 4
+	#define MAX_DATA_ITEMS		 14
+#endif
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
@@ -38,13 +53,25 @@ static void timer0_handler(struct k_timer *dummy)
 
 K_TIMER_DEFINE(timer0, timer0_handler, NULL);
 
+#ifdef CONFIG_INTER_L1_MESSAGE_QUEUE
 typedef struct {
-    uint32_t x_reading;
-    uint32_t y_reading;
-    uint32_t z_reading;
+	uint32_t x_reading;
+	uint32_t y_reading;
+	uint32_t z_reading;
 } SensorReading;
 
 K_MSGQ_DEFINE(device_message_queue, sizeof(SensorReading), 16, 4);
+#endif
+
+#ifdef CONFIG_INTER_L1_FIFO
+struct data_item_t {
+	void *fifo_reserved;
+	uint8_t  data[MAX_DATA_SIZE];
+	uint16_t len;
+}; 
+
+K_FIFO_DEFINE(my_fifo);
+#endif
 
 int main(void)
 {
@@ -73,20 +100,48 @@ static void producer_func(void *unused1, void *unused2, void *unused3)
 	ARG_UNUSED(unused1);
 	ARG_UNUSED(unused2);
 	ARG_UNUSED(unused3);
+#ifdef CONFIG_INTER_L1_FIFO
+	static uint32_t dataitem_count = 0;
+#endif
 
 	while (1) {
+#ifdef CONFIG_INTER_L1_MESSAGE_QUEUE
 		static SensorReading acc_val = {100, 100, 100};
 		int ret;
 		
 		ret = k_msgq_put(&device_message_queue,&acc_val,K_FOREVER);
-        if (ret){
-            LOG_ERR("Return value from k_msgq_put = %d",ret);
-        }
+		if (ret){
+			LOG_ERR("Return value from k_msgq_put = %d",ret);
+		}
 
 		acc_val.x_reading += 1;
 		acc_val.y_reading += 1;
 		acc_val.z_reading += 1;
 		k_msleep(PRODUCER_SLEEP_TIME_MS);
+#endif
+
+#ifdef CONFIG_INTER_L1_FIFO
+		int bytes_written;
+		/* Generate a random number between MIN_DATA_ITEMS & MAX_DATA_ITEMS to represent the number
+		of data items to send every time the producer thread is scheduled */
+		uint32_t data_number = MIN_DATA_ITEMS + sys_rand32_get() % (MAX_DATA_ITEMS - MIN_DATA_ITEMS + 1);
+		for (int i = 0; i < data_number; i++) {
+			/* Create a data item to send */
+			struct data_item_t *buf = k_malloc(sizeof(struct data_item_t));
+			if (buf == NULL) {
+				/* Unable to locate memory from the heap */
+				LOG_ERR("Unable to allocate memory");
+				return;
+			}
+			bytes_written = snprintf(buf->data, MAX_DATA_SIZE, "Data Seq. %u:\t%u",
+						dataitem_count, sys_rand32_get());
+			buf->len = bytes_written;
+			dataitem_count++;
+			k_fifo_put(&my_fifo, buf);
+		}
+		LOG_INF("Producer: Data Items Generated: %u", data_number);
+		k_msleep(PRODUCER_SLEEP_TIME_MS);
+#endif
 	}
 }
 
@@ -97,6 +152,7 @@ static void consumer_func(void *unused1, void *unused2, void *unused3)
 	ARG_UNUSED(unused3);
 
 	while (1) {
+#ifdef CONFIG_INTER_L1_MESSAGE_QUEUE
 		SensorReading temp;
 		int ret;
 		
@@ -105,8 +161,15 @@ static void consumer_func(void *unused1, void *unused2, void *unused3)
             LOG_ERR("Return value from k_msgq_get = %d",ret);
         }
 
-		LOG_INF("Values got from the queue: %d.%d.%d\r\n", temp.x_reading, temp.y_reading,
-			temp.z_reading);
+		LOG_INF("Values got from the queue: %d.%d.%d\r\n", temp.x_reading, temp.y_reading,temp.z_reading);
+#endif
+
+#ifdef CONFIG_INTER_L1_FIFO
+		struct data_item_t *rec_item;
+        rec_item = k_fifo_get(&my_fifo, K_FOREVER);
+        LOG_INF("Consumer: %s\tSize: %u",rec_item->data,rec_item->len);
+        k_free(rec_item);
+#endif
 	}
 }
 
