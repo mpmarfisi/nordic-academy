e820b5db59ad22be53e2a5249637680f961a6b4e
Matias Marfisi <matimarfisi99@gmail.com>
2026-07-01 19:18:34 +0000

    feat: add l2 e2 coredump debugging exercise

--- sdk_intermediate/l2/src/main.c
+++ sdk_intermediate/l2/src/main.c
@@ -4,23 +4,56 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <dk_buttons_and_leds.h>
 
-LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
-int8_t test_var = 124;
+#include <zephyr/debug/coredump.h>
+
+
+LOG_MODULE_REGISTER(Lesson2_Exercise2, LOG_LEVEL_INF);
+
+void crash_function(uint32_t *addr)
+{
+	LOG_INF("Button pressed at %" PRIu32, k_cycle_get_32());
+	LOG_INF("Coredump: %s", CONFIG_BOARD);
+
+	#if !defined(CONFIG_CPU_CORTEX_M)
+	/* For null pointer reference */
+	*addr = 0;
+	#else
+		ARG_UNUSED(addr);
+		/* Dereferencing null-pointer in TrustZone-enabled
+	 	* builds may crash the system, so use, instead an
+	 	* undefined instruction to trigger a CPU fault.
+	 	*/
+	__asm__ volatile("udf #0" : : : );
+#endif
+}
+
+void button_handler(uint32_t button_state, uint32_t has_changed)
+{
+	if (has_changed & button_state)
+	{
+		switch (has_changed)
+		{
+            case DK_BTN1_MSK:
+                LOG_INF("Button 1 pressed");
+				crash_function(0);
+                break;
+			default:
+				break;
+		}
+    }
+}
 
 int main(void)
 {
+    if (dk_buttons_init(button_handler)) {
+        LOG_ERR("Failed to initialize the buttons library");
+    }
 
-	LOG_INF("Starting Exercise 1!");
-
-	for (int i = 0; i < 10; i++)
-	{
-		test_var = test_var + 1;
-		LOG_INF("test_var = %d",test_var);
-	}
+	LOG_INF("Press button 1 to get a fault error");
 
 	return 0;
 }
