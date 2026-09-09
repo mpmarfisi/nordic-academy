32151316b544bffb9e9de20256c0245990408811
Matias Marfisi <matimarfisi99@gmail.com>
2026-09-09 21:43:20 +0000

    feat: add all 4 dk leds to use custom blink driver

--- sdk_intermediate/l7/app_led/src/main.c
+++ sdk_intermediate/l7/app_led/src/main.c
@@ -6,6 +6,7 @@
 #include <zephyr/kernel.h>
 #include <blink.h>
 #include <zephyr/logging/log.h>
+
 LOG_MODULE_REGISTER(Lesson7_Exercise3, LOG_LEVEL_INF);
 
 #define BLINK_PERIOD_MS_STEP 100U
@@ -13,22 +14,30 @@ LOG_MODULE_REGISTER(Lesson7_Exercise3, LOG_LEVEL_INF);
 
 int main(void)
 {
-/* Start blinking - slow*/ 
-unsigned int period_ms = BLINK_PERIOD_MS_MAX;
+    /* Start blinking - slow*/ 
+    unsigned int period_ms = BLINK_PERIOD_MS_MAX;
 
     LOG_INF("Zephyr Example Application");
 
-    const struct device * blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-    if (!device_is_ready(blink)) {
-        LOG_ERR("Blink LED not ready");
-        return 0;
-    }
+    const struct device * blink[4];
+    blink[0]  = DEVICE_DT_GET(DT_NODELABEL(blink_led0));
+    blink[1]  = DEVICE_DT_GET(DT_NODELABEL(blink_led1));
+    blink[2]  = DEVICE_DT_GET(DT_NODELABEL(blink_led2));
+    blink[3]  = DEVICE_DT_GET(DT_NODELABEL(blink_led3));
+    
+    int ret = 0;
+    for (int i = 0; i < 4; i++) {
+        if (!device_is_ready(blink[i])) {
+            LOG_ERR("Blink LED %d not ready", i);
+            return 0;
+        }
 
-    /* Use custom API to turn LED off */
-    int ret = blink_off(blink);
-    if (ret < 0) {
-        LOG_ERR("Could not turn off LED (%d)", ret);
-        return 0;
+        /* Use custom API to turn LED off */
+        ret = blink_off(blink[i]);
+        if (ret < 0) {
+            LOG_ERR("Could not turn off LED %d (%d)", i, ret);
+            return 0;
+        }
     }
 
     while (1) {
@@ -41,8 +50,13 @@ unsigned int period_ms = BLINK_PERIOD_MS_MAX;
 
         printk("Setting LED period to %u ms\n", period_ms);
         /* Use custom API to change LED blinking period*/
-        blink_set_period_ms(blink, period_ms);
-        
+        for (int i = 0; i < 4; i++) {
+            ret = blink_set_period_ms(blink[i], period_ms);
+            if (ret < 0) {
+                LOG_ERR("Could not set LED %d period (%d)", i, ret);
+                return 0;
+            }
+        }
         k_sleep(K_MSEC(2000));
     }
 
